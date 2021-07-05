c4f8adb366f118f32186052ba788388f6ef8dcd0
Nora C <nocorser@gmail.com>
2021-07-05 14:59:26 -0500

    initial commit -- imports + setup codegrade

--- api/server.js
+++ api/server.js
@@ -1,3 +1,7 @@
 // BUILD YOUR SERVER HERE
+const express = require('express'); // import the express package
+const User = require('./users/model'); // import users
+const server = express(); // creates the server
+server.use(express.json()); // makes server parse json
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+module.exports = server; // EXPORT YOUR SERVER instead of {}
