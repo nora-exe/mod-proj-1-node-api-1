b68e5d9e653d97fa2a010c8bde8daada1b4f258b
Nora C <nocorser@gmail.com>
2021-07-05 16:10:43 -0500

    get by id, post

--- api/server.js
+++ api/server.js
@@ -20,12 +20,42 @@ server.get('/api/users', (req, res) => {
 
 //🔽 GET user by id
 server.get('/api/users/:id', (req, res) => {
-    
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+                err: err.message
+            })
+        })
 })
 
 //🔽 POST (add) user
-server.get('api/users', (req, res) => {
-    
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+        User.insert(user)
+            .then(newUser => {
+                res.status(201).json(newUser);
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'There was an error while saving the user to the database',
+                    err: err.message
+                })
+        })
+    }
 })
 
 //🔽 DEL user
